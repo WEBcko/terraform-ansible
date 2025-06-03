12b4b4f11f61765462325c23765b115c816ec549
Roberto Vinicius Passos <31169609+RvPassos@users.noreply.github.com>
2025-06-03 14:33:57 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,7 @@ provider "google" {
   project = var.project_id
   region  = var.region
   zone    = var.zone
+  credentials = file(var.credentials_file_path)
 }
 
 resource "google_compute_instance" "vm_instance" {
@@ -30,15 +31,51 @@ resource "google_compute_instance" "vm_instance" {
   tags = ["ssh", "app"]
 }
 
-resource "google_compute_firewall" "allow_ssh_http" {
-  name    = "allow-ssh-http"
-  network = "default"
+#resource "google_compute_firewall" "allow_ssh_http" {
+ # name    = "allow-ssh-http"
+  #network = "default"
 
-  allow {
-    protocol = "tcp"
-    ports    = ["22", "80", "3000", "8000"]
+  #allow {
+   # protocol = "tcp"
+   # ports    = ["22", "80", "3000", "8000"]
+  #}
+
+  #source_ranges = ["0.0.0.0/0"]
+  #target_tags   = ["ssh", "app"]
+#}
+
+
+
+
+
+provider "google" {
+  project     = var.project_id
+  region      = var.region
+  zone        = var.zone
+  credentials = file(var.credentials_file_path)
+}
+
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "dev-vm"
+  machine_type = var.machine_type  
+  zone         = var.zone         
+
+  boot_disk {
+    initialize_params {
+      image = var.image         
+      size  = var.disk_size       
+    }
   }
 
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["ssh", "app"]
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"  
+  }
+
+  tags = ["ssh", "app"]
 }
